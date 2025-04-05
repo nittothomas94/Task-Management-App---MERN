7ce76071b4c9d29d17e7512466196992f98e1b95
nittothomas <nittothomas94@example.com>
2025-04-05 17:23:05 +0530

    port edited

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middlewares
 app.use(cors());
